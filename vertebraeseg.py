5493c64ea57fe4a95ac1bfc834556867c626a804
Batuhan Kursat Unal <unalbatuhan98@hotmail.com>
2024-07-05 21:55:27 +0200

    updated method for sorting the imported list of the dataset

--- vertebraeseg.py
+++ vertebraeseg.py
@@ -53,18 +53,30 @@ mr_imagefiles = []
 ## To get rid of the redundant data
 for file in mr_images:
     if file[0].isnumeric():
-        mr_imagefiles += [file]
+        mr_imagefiles.append(file)
         
-## Sorting out the lists to have a ordered size list
-### Extracting the numeric part from the filename
-def extract_number(mri_files):
-    match = re.match(r"(\d+)_", mri_files)
-    if match:
-        return int(match.group(1)) 
-    else: 
-        float('inf')
+def extract_number_and_modality(mri_file):
+    '''
+    Key function to determine sorting dependencies
 
-sorted_mr_imagefiles = sorted(mr_imagefiles, key=extract_number)
+    Parameters
+    ----------
+    mri_file : Files of the dataset stored in your local machine
+
+    Returns
+    -------
+    subject_number : Subject number, i.e. first integer in data name
+    modality_priority: Priority determined by the way data is stored in the folder
+
+    '''
+    match = re.match(r"(\d+)_(t1|t2_SPACE|t2)\.mha", mri_file)
+    subject_number = int(match.group(1))
+    modality = match.group(2)
+    # Assigning different priority to each modality to enforce the same order as in the folder
+    modality_priority = {"t1": 1, "t2_SPACE": 2, "t2": 3}
+    return subject_number, modality_priority[modality]
+
+sorted_mr_imagefiles = sorted(mr_imagefiles, key=extract_number_and_modality)
     
 for files in sorted_mr_imagefiles:    
     mr_images_path = os.path.join(mr_volume_dir, files)

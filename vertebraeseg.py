e8e599e665932c79efc07d00576c3de59dc2ea5d
Batuhan Kursat Unal <unalbatuhan98@hotmail.com>
2024-07-25 21:23:24 +0300

    preprocessing pipeline added

--- vertebraeseg.py
+++ vertebraeseg.py
@@ -247,3 +247,29 @@ class CropNonspinalVoxels:
     
         return cropped_mr_im
     
+    
+# Preprocessing Pipeline    
+norm_type = input('Determine the type of normalizaion (minmax or zscore):')
+for files in sorted_mr_imagefiles:
+    mr_images_path = os.path.join(mr_volume_dir, files)
+    #Z-score Normalization
+    if __name__ == "__main__" and norm_type == 'zscore':
+        mr_image_path1 = mr_images_path
+        normalizer = IntensityNormalizer(mr_image_path1)
+        znormalized_image, znormalized_array = normalizer.zscore_normalizer() #Z-score normalization
+        mr_image = znormalized_image
+        mr_array = znormalized_array
+        
+    #Min-max normalization
+    if __name__ == "__main__" and norm_type == 'minmax':
+        mr_image_path1 = mr_images_path
+        normalizer = IntensityNormalizer(mr_image_path1)
+        normalized_image, normalized_array = normalizer.minmax_normalizer() #Min-max normalization
+        mr_image = normalized_image
+        mr_array = normalized_array    
+    
+    #Cropping out
+    cropper = CropNonspinalVoxels(mr_image, mr_array)
+    cropped_mr_image = cropper.crop_nonspinal()
+    output_path = 'pathtooutput/' + 'cropped_' + files  #Replace with the desired output path
+    sitk.WriteImage(cropped_mr_image, output_path)
